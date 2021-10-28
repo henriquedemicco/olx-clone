b0130cb5e33ad382574774076a4a847e51503082
Miccao <henriquedemicco@hotmail.com>
2021-10-27 22:38:58 -0300

    Corrigindo url para POST via Axios

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -37,7 +37,7 @@ const Form = () => {
                 'content-type': 'multipart/form-data'
             },
             method: 'post',
-            url: 'http://api-olx-clone.herokuapp.com/ad',
+            url: 'http://api-olx-clone.herokuapp.com/ad/',
             data: fd,
         });
     }
